81cdf84d4023abbe921ed88a4921dc670fb8c755
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2020-10-18 17:23:08 +0700

    RekWeb codeigniter

---
+++ app/Views/pages/about.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia saepe nihil voluptates ratione, dolores, nobis blanditiis explicabo minima ipsum est, repellendus suscipit. Excepturi quo ipsum quibusdam, ea cum nihil laborum!</p>
+        </div>
+    </div>
+</div>
