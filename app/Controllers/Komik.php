e373236d3afbab6d541f9485949fb4e90a03b816
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2020-10-19 00:31:29 +0700

    RekWeb codeigniter

--- app/Controllers/Komik.php
+++ app/Controllers/Komik.php
@@ -14,16 +14,24 @@ class Komik extends BaseController
 
     public function index()
     {
-        $komik = $this->komikModel->findAll();
+        // $komik = $this->komikModel->findAll();
         $data = [
             'title' => 'Daftar Komik',
-            'komik' => $komik
+            'komik' => $this->komikModel->getKomik()
         ];
-
         // $komikModel = new \App\Models\KomikModel();
 
-
-
         return view('komik/index', $data);
+    }
+
+
+    public function detail($slug)
+    {
+        $komik = $this->komikModel->getKomik($slug);
+        $data = [
+            'title' => 'Detail Komik',
+            'komik' => $this->komikModel->getKomik($slug)
+        ];
+        return view('komik/detail', $data);
     }
 }
