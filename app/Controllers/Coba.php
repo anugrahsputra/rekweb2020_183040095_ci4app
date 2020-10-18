29d9c7c2ef5044869d8e8170d58e21988b6395a9
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2020-10-18 16:18:47 +0700

    RekWeb codeigniter

---
+++ app/Controllers/Coba.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers;
+
+class Coba extends BaseController
+{
+    public function index()
+    {
+        return view('welcome_message');
+    }
+
+    public function coba()
+    {
+        echo "Hello world!";
+    }
+
+    //--------------------------------------------------------------------
+
+}
